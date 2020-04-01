02ac2aae154ddb6eed27d60d65ba9feee85917d4
LorenVB <vidalbellon@hotmail.es>
2020-04-02 00:28:00 +0200

    Pipes In Progress

---
+++ Temas Curso Angular- Fernando/03-Pipes/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    nombre:string="Capitán América";
+    arreglo=[1,2,3,4,5,6,7,8];
+    PI: number= Math.PI;
+
+
+}
+
